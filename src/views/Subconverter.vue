3062955ff236552200af33f8b0329c898d5f358b
AGWA5783 <58136441+AGWA5783@users.noreply.github.com>
2021-12-18 01:09:41 +0800

    SCV Delete

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -333,7 +333,6 @@ export default {
         sort: false,
         udp: false,
         tfo: false,
-        scv: false,
         fdn: false,
         appendType: false,
         insert: false, // 是否插入默认订阅的节点，对应配置项 insert_url
